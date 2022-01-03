f876936b463c64bdb03250ee38cfd178d5ce804d
moisesmiiranda <moisesmiranda1994@gmail.com>
2022-01-03 08:16:43 -0300

    adicionando mais exemplos de uso da stream api

---
+++ src/streamExemplos/Main.java
@@ -0,0 +1,51 @@
+package streamExemplos;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+	private static List<String> itens = Arrays.asList("computador", "papel", "caneta", "cortina", "gaveta");
+	
+	public static void main(String[] args) {
+		//executar1();
+		//executar2();
+		executar3();
+	}
+	
+	private static void executar1() {
+		/*
+		 * 1. Ordenar
+		 * 2. Manter somente os 3 primeiros elementos
+		 * 3. Mostrar o resultado na tela 
+		 */
+		
+		itens.stream().sorted().limit(3).forEach(System.out::println);
+	}
+	
+	private static void executar2() {
+		/*
+		 * 1. Filtrar apenas elementos que contém a letra 'C'
+		 * 2. Montar uma nova lista com estes elementos
+		 * */
+		
+		List<String> itensFiltrados = itens.stream()
+				.filter(e -> e.contains("c"))
+				.collect(Collectors.toList());
+		
+		System.out.println(itensFiltrados);
+	}
+	
+	private static void executar3() {
+		/*
+		 * 1. Somar a quantidade de caracteres de todos os elementos
+		 * 2. Retornar a soma
+		 * */
+		int count = itens.stream()
+				.collect(Collectors.summingInt(e -> e.length()));
+		
+		System.out.println(count);
+	}
+	
+}
